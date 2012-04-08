510807c9e40d78ea698893467eab7e122f1d8f5c
Christoph Haag <haagch.christoph@googlemail.com>
2012-04-08 18:10:21 +0200

    developer manual: ExampleAlgorithm.java was a duplicate of the Factory... This is the correct file

--- DeveloperManual/ExampleAlgorithm.java
+++ DeveloperManual/ExampleAlgorithm.java
@@ -1,65 +1,51 @@
 package de.tourenplaner.algorithms;
 
+import de.tourenplaner.computecore.ComputeRequest;
+import de.tourenplaner.computecore.RequestPoints;
+import de.tourenplaner.computecore.Way;
 import de.tourenplaner.graphrep.GraphRep;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-public class ExampleAlgorithmFactory extends GraphAlgorithmFactory {
-    private ArrayList<Map<String, Object>> constraints;
-    private ArrayList<Map<String, Object>> pointConstraints;
-    private HashMap<String, Object>        details;
-
-    public ExampleAlgorithmFactory(GraphRep graph) {
+public class ExampleAlgorithm extends GraphAlgorithm {
+    public ExampleAlgorithm(GraphRep graph) {
+        // the abstract class stores the graph representaion for us
         super(graph);
-        // We might as well just return null instead of empty lists for constraints and pointconstraints
-        constraints = new ArrayList<Map<String, Object>>(0);
-        pointConstraints = new ArrayList<Map<String, Object>>(0);
-        details = new HashMap<String, Object>(3);
-        details.put("hidden", this.isHidden());
-        details.put("minpoints", 2);
-        details.put("sourceistarget", false);
     }
 
+    // the compute method is called for each request for this algorithm. Make sure not to rely on fields defined outside this method to be empty
     @Override
-    public List<Map<String, Object>> getPointConstraints() {
-        return this.pointConstraints;
-    }
+    public void compute(ComputeRequest req) throws ComputeException {
+        // that should never happen but better be safe than sorry
+        assert req != null : "We ended up without a request object in run";
 
-    @Override
-    public List<Map<String, Object>> getConstraints() {
-        return this.constraints;
-    }
+        // this is the array with points that the client sent us
+        RequestPoints points = req.getPoints();
 
-    @Override
-    public Map<String, Object> getDetails() {
-        return details;
-    }
+        // these are the constraints the client sent us
+        Map<String, Object> constraints = req.getConstraints();
 
-    @Override
-    public Algorithm createAlgorithm() {
-        return new ExampleAlgorithm(graph);
-    }
+        if (points.size() < 2) {
+            // a ComputeException will cause an error message to be sent to the client
+            throw new ComputeException("Not enough points, need at least 2");
+        }
 
-    @Override
-    public String getURLSuffix() {
-        return "example";
-    }
+        Way resultway = new Way();
 
-    @Override
-    public String getAlgName() {
-        return "An example Algorithm";
-    }
+        // For this example we just reverse the order of the points the client sent us
+        for (int i = points.size() - 1; i >= 0;  i--) {
+            resultway.addPoint(points.getPointLat(i), points.getPointLon(i));
+        }
 
-    @Override
-    public int getVersion() {
-        return 1;
-    }
+        // We add our "result" to the (empty) list of ways that will be sent to the client
+        req.getResultWays().add(resultway);
 
-    @Override
-    public boolean isHidden() {
-        return false;
+        // We write some information in misc. In a real algorithm we would use the graph representation for more :)
+        HashMap<String, Object> misc = new HashMap<String, Object>();
+        misc.put("Reversed Points", points.size());
+        misc.put("Nodes in graph", graph.getNodeCount());
+        misc.put("Edges in graph", graph.getEdgeCount());
+        req.setMisc(misc);
     }
 }
